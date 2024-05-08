53d0255be6ac8f6d706658288cc394f430673e4b
Josh Vlk <josh@vlkpack.com>
2024-05-08 14:35:45 -0400

    chore: config changes

--- rescript/classnames/vite.config.ts
+++ rescript/classnames/vite.config.ts
@@ -14,9 +14,8 @@ export default defineConfig({
     cache: {
       dir: '../../node_modules/.vitest',
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
+    environment: 'node',
+    include: ['__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
       reportsDirectory: '../../coverage/rescript/classnames',
