71bfff0a33d6785aa3f41a7a80229e6d6cb4448b
Josh Vlk <josh@vlkpack.com>
2024-05-08 13:21:28 -0400

    chore: example unit test

--- rescript/classnames/vite.config.mjs
+++ rescript/classnames/vite.config.mjs
@@ -1,23 +1,16 @@
 import { defineConfig } from 'vite';
 
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/rescript/classnames',
 
-  plugins: [nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+  plugins: [],
 
   test: {
     globals: true,
     cache: { dir: '../../node_modules/.vitest' },
     environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
       reportsDirectory: '../../coverage/rescript/classnames',
