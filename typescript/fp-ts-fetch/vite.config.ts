a05b6123fc3555971b406205f3ca578d9147603c
Josh Vlk <josh@vlkpack.com>
2024-05-10 10:04:32 -0400

    chore: update scripts to run nx

--- typescript/fp-ts-fetch/vite.config.ts
+++ typescript/fp-ts-fetch/vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts"
 export default defineConfig({
   plugins: [
     dts({
-      tsconfigPath: "./tsconfig.build.json",
+      tsconfigPath: "./tsconfig.lib.json",
     }),
   ],
   build: {
