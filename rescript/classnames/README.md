78045297e104d63b20223a3807ca6c24468a48f4
Josh Vlk <josh@vlkpack.com>
2024-05-14 15:15:08 -0400

    chore: remove rescript packages

--- rescript/classnames/README.md
+++
@@ -1,2 +0,0 @@
-# @jvlk/rescript-classnames
-A ReScript version of [classnames](https://www.npmjs.com/package/classnames).
--- rescript/classnames/__tests__/classnames.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest"
-import { classnames } from "../src/classnames.gen"
-
-test("classnames", () => {
-  expect(classnames(["foo", "bar"])).toEqual("foo bar")
-  expect(classnames(["foo"], { bar: true })).toEqual("foo bar")
-  expect(classnames(["foo"], { bar: false })).toEqual("foo")
-})
